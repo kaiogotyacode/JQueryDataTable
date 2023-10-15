9a8cd63504b67e8b26076c6d379be00dbc0373c4
Kaio Maciel <43120150+kaiogotyacode@users.noreply.github.com>
2023-10-15 19:26:18 -0300

    Added font-awesome.

--- JQueryDataTable/Controllers/HomeController.cs
+++ JQueryDataTable/Controllers/HomeController.cs
@@ -1,32 +1,21 @@
-﻿using JQueryDataTable.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace JQueryDataTable.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+    {     public IActionResult Index()
         {
-            _logger = logger;
-        }
+            return View();
+        }       
 
-        public IActionResult Index()
+        public IActionResult Create()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        
+        public IActionResult Edit()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
